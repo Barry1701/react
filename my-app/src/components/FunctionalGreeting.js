011205f69ade96f01e9d21f122839596908f3ab2
Bartosz Stanczuk <synergy170185@gmail.com>
2024-08-13 16:08:38 +0000

    initial commit - componenets ready

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const FunctionalGreeting = () =>   <h1>Chuj ci w dupe!</h1>;
+   
+
+
+export default FunctionalGreeting;
---
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const FunctionalGreetingWithProps = () =>   <h1>I Co Chuju!</h1>;
+   
+
+
+export default FunctionalGreetingWithProps;
